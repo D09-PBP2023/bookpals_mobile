196bd670f2a91b111a4e44841f334cb4ae634d87
marvelm57 <vivalamarvel@gmail.com>
2023-12-11 20:57:42 +0700

    create custom Review model and implement fetching data from Django

--- lib/features/review/screens/list_review.dart
+++ lib/features/review/screens/list_review.dart
@@ -1,97 +1,137 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:bookpals_mobile/features/review/models/review.dart';
 import 'package:bookpals_mobile/core/theme/font_theme.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:intl/intl.dart';
+import '../../../core/bases/models/Book.dart';
 
 
 class ReviewPage extends StatefulWidget {
-    const ReviewPage({super.key});
+    final Book book; // Accept Book as a parameter
+
+    const ReviewPage({Key? key, required this.book}) : super(key: key);
 
     @override
     State<ReviewPage> createState() => _ReviewPageState();
 }
 
 class _ReviewPageState extends State<ReviewPage> {
-    @override
-    Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Review List'),
-      ),
-      body: ListView.builder(
-        itemCount: 5, // Ganti dengan jumlah review yang Anda miliki
-        itemBuilder: (context, index) {
-          return ReviewCard();
-        },
-      ),
+  Future<List<Review>> fetchReview() async {
+    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+    var url = Uri.parse(
+        'https://bookpals-d09-tk.pbp.cs.ui.ac.id/get_review/${widget.book.pk}/');
+    var response = await http.get(
+        url,
+        headers: {"Content-Type": "application/json"},
     );
-  }
-}
 
-class ReviewCard extends StatelessWidget {
+    // melakukan decode response menjadi bentuk json
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // melakukan konversi data json menjadi object Product
+    List<Review> list_review = [];
+    for (var d in data) {
+        if (d != null) {
+            list_review.add(Review.fromJson(d));
+        }
+    }
+    return list_review;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${widget.book.fields.name}'),
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Profile Picture
-          CircleAvatar(
-            // Ganti dengan gambar profil
-            backgroundImage: AssetImage('assets/profile_picture.png'),
-            radius: 30,
-          ),
-          SizedBox(width: 16),
-          // Informasi Review
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Text "Review by username"
-                Text(
-                  'Review by username',
-                  style: FontTheme.blackCaptionBold(),
-                ),
-                SizedBox(height: 8),
-                // Rating Bar
-                IgnorePointer(
-                  child: RatingBar.builder(
-                    initialRating: 3,
-                    itemCount: 5,
-                    itemSize: 20,
-                    itemBuilder: (context, _) => Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                    ),
-                    onRatingUpdate: (rating) {
-                      // Do nothing or provide a dummy function
-                    },
+      body: FutureBuilder(
+        future: fetchReview(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (!snapshot.hasData) {
+              return const Column(
+                children: [
+                  Text(
+                    "Tidak ada data produk.",
+                    style:
+                      TextStyle(color: Color(0xff59A5D8), fontSize: 20),
                   ),
-                ),
-                SizedBox(height: 8),
-                // Tanggal
-                Text(
-                  '2023-12-06',
-                  style: FontTheme.blackCaption(),
-                ),
-                
-                SizedBox(height: 8),
-                // Lorem Ipsum Text
-                Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at malesuada massa, id vestibulum magna. Curabitur dapibus condimentum felis et mattis. Maecenas efficitur nulla orci, nec placerat erat pellentesque non. Quisque aliquet convallis quam, ac finibus sem',
-                  style: FontTheme.blackCaption(),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) => 
+                Container(
+                  margin: EdgeInsets.all(8),
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Profile Picture
+                      CircleAvatar(
+                      // Ganti dengan gambar profil
+                        backgroundImage: AssetImage('assets/profile_picture.png'),
+                        radius: 30,
+                      ),
+                      SizedBox(width: 16),
+                      // Informasi Review
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            // Text "Review by username"
+                            Text(
+                              'Review by ${snapshot.data![index].user}',
+                              style: FontTheme.blackCaptionBold(),
+                            ),
+                            SizedBox(height: 8),
+                            // Rating Bar
+                            IgnorePointer(
+                              child: RatingBar.builder(
+                                initialRating: snapshot.data![index].rating,
+                                itemCount: 5,
+                                itemSize: 20,
+                                itemBuilder: (context, _) => Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                onRatingUpdate: (rating) {
+                                  // Do nothing or provide a dummy function
+                                },
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            // Tanggal
+                            Text(
+                              DateFormat('yyyy-MM-dd').format(snapshot.data![index].created_at),
+                              style: FontTheme.blackCaption(),
+                            ),       
+                            SizedBox(height: 8),
+                            // Review Text
+                            Text(
+                              '${snapshot.data![index].review}',
+                              style: FontTheme.blackCaption(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              );      
+            }
+          }
+        }        
+      )
+    );  
   }
 }
--- lib/features/review/screens/review_form.dart
+++ lib/features/review/screens/review_form.dart
@@ -6,140 +6,136 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import '../../../core/bases/models/Book.dart';
 
 import '../../../core/bases/widgets/scaffold.dart';
 import '../../../core/bases/widgets/text_field.dart';
 
 class ReviewFormPage extends StatefulWidget {
-  const ReviewFormPage({super.key});
+  final Book book; // Accept Book as a parameter
+
+  const ReviewFormPage({Key? key, required this.book}) : super(key: key);
 
   @override
   State<ReviewFormPage> createState() => _ReviewFormPageState();
 }
 
 class _ReviewFormPageState extends State<ReviewFormPage> {
-  // final TextEditingController _ratingController = TextEditingController();
   final TextEditingController _reviewController = TextEditingController();
-  // bool isLoading = false;
-  // late String errorMessage = "";
-  final _formKey = GlobalKey<FormState>();
   double _rating = 0;
+  bool isLoading = false;
+  late String errorMessage = "";
 
   @override
   Widget build(BuildContext context) {
-    // final auth = context.watch<AuthProvider>();
-
-    return BpScaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${widget.book.fields.name}'),
+      ),
       resizeToAvoidBottomInset: true,
-      body: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxHeight: MediaQuery.of(context).size.height,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 30),
-                // errorMessage.isNotEmpty
-                //     ? Center(
-                //         heightFactor: 2,
-                //         child: Text(
-                //           errorMessage,
-                //           style: FontTheme.redSubtitle(),
-                //         ),
-                //       )
-                //     : const Offstage(),
-                Text(
-                  "Rating",
-                  style: FontTheme.blackSubtitle(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(32.0),
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30),
+              Text(
+                "Rating",
+                style: FontTheme.blackSubtitle(),
+              ),
+              const SizedBox(height: 8),
+              RatingBar.builder(
+                initialRating: _rating,
+                minRating: 1,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemSize: 40,
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.amber,
                 ),
-                const SizedBox(height: 8),
-                // Replace the username TextField with a RatingBar
-                // You can use any RatingBar package you prefer
-                // Here, I'm assuming a fictional RatingBar widget
-                RatingBar.builder(
-                  initialRating: 0,
-                  minRating: 1,
-                  direction: Axis.horizontal,
-                  allowHalfRating: true,
-                  itemCount: 5,
-                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                  itemBuilder: (context, _) => Icon(
-                    Icons.star,
-                    color: Colors.amber,
-                  ),
-                  onRatingUpdate: (rating) {
-                    // Tangkap nilai rating dan simpan di variabel atau state
-                    setState(() {
-                      _rating = rating!;
-                    });
-                    
-                  },
-                ),
-                const SizedBox(height: 16),
-                Text(
-                  "Review",
-                  style: FontTheme.blackSubtitle(),
-                ),
-                const SizedBox(height: 8),
-                // Modify the password TextField to be a multi-line TextField
-                BookpalsTextField(
-                  title: "Review",
-                  hint: "Masukkan review",
-                  controller: _reviewController,
-                ),
-                const SizedBox(height: 32),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 60,
-                      width: 200,
-                      child: BpButton(
-                        text: "Add Review",
-                        // onTap: () async {
-                        //   if (_ratingController.text.isEmpty ||
-                        //       _reviewController.text.isEmpty) {
-                        //     setState(() {
-                        //       errorMessage =
-                        //           "Rating dan review harus di-isi!";
-                        //     });
-                        //     return;
-                        //   }
-
-                        //   setState(() {
-                        //     isLoading = true;
-                        //   });
-
-                        //   final res = await auth.signIn(
-                        //       _ratingController.text, _reviewController.text);
-
-                        //   setState(() {
-                        //     isLoading = false;
-                        //   });
-
-                        //   if (res["status"]) {
-                        //     Navigator.pushReplacement(
-                        //       context,
-                        //       MaterialPageRoute(
-                        //           builder: (context) => const HomePage()),
-                        //     );
-                        //   } else {
-                        //     setState(() {
-                        //       errorMessage = res["message"];
-                        //     });
-                        //   }
-                        // },
+                onRatingUpdate: (rating) {
+                  _rating = rating;
+                },
+              ),
+              const SizedBox(height: 16),
+              errorMessage.isNotEmpty
+                  ? Center(
+                      heightFactor: 2,
+                      child: Text(
+                        errorMessage,
+                        style: FontTheme.redSubtitle(),
                       ),
+                    )
+                  : const Offstage(),
+              Text(
+                "Review",
+                style: FontTheme.blackSubtitle(),
+              ),
+              const SizedBox(height: 8),
+              BookpalsTextField(
+                title: "Review",
+                hint: "Enter your review",
+                controller: _reviewController,
+              ),
+              const SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 60,
+                    width: 200,
+                    child: BpButton(
+                      text: "Submit",
+                      // isLoading: isLoading,
+                      onTap: () async {
+                        if (_rating == 0) {
+                          setState(() {
+                            errorMessage = "Please give a rate";
+                          });
+                          return;
+                        }
+                        // if (_usernameController.text.isEmpty ||
+                        //     _passwordController.text.isEmpty) {
+                        //   setState(() {
+                        //     errorMessage =
+                        //         "Username dan password harus di-isi!";
+                        //   });
+                        //   return;
+                        // }
+
+                        // setState(() {
+                        //   isLoading = true;
+                        // });
+
+                        // final res = await auth.signIn(
+                        //     _usernameController.text, _passwordController.text);
+
+                        // setState(() {
+                        //   isLoading = false;
+                        // });
+
+                        // if (res["status"]) {
+                        //   Navigator.pushReplacement(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => const HomePage()),
+                        //   );
+                        // } else {
+                        //   setState(() {
+                        //     errorMessage = res["message"];
+                        //   });
+                        // }
+                      },
                     ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+            ],
           ),
         ),
       ),
