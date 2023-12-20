a6a5406958484d4a1b0e33951d76d660c271892d
ArvinCS <arvinciu86@yahoo.com>
2023-12-20 14:24:09 +0700

    fix: endpoint to deployment

--- lib/core/environments/endpoints.dart
+++ lib/core/environments/endpoints.dart
@@ -1,6 +1,6 @@
 class Endpoints {
   static const baseUrl =
-      'http://10.0.2.2:8000'; // Or change to 'https://bookpals-d09-tk.pbp.cs.ui.ac.id' for production
+      'https://bookpals-d09-tk.pbp.cs.ui.ac.id'; // Or change to 'https://bookpals-d09-tk.pbp.cs.ui.ac.id' for production
   static const loginUrl = '$baseUrl/login-mobile/';
   static const registerUrl = '$baseUrl/register-mobile/';
   static const logoutUrl = '$baseUrl/logout-mobile/';
