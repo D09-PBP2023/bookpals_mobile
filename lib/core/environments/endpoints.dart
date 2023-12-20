65ebcb8ec7e0993bacc51191258b2e931a37de3f
rxa15 <114054405+rxa15@users.noreply.github.com>
2023-12-20 23:05:51 +0700

    Final revision

--- lib/core/environments/endpoints.dart
+++ lib/core/environments/endpoints.dart
@@ -1,5 +1,5 @@
 class Endpoints {
-  static const baseUrl = 'http://127.0.0.1:8000';
+  static const baseUrl = 'https://bookpals-d09-tk.pbp.cs.ui.ac.id';
   // 'http://10.0.2.2:8000'; // Or change to 'https://bookpals-d09-tk.pbp.cs.ui.ac.id' for production
   static const loginUrl = '$baseUrl/login-mobile/';
   static const registerUrl = '$baseUrl/register-mobile/';
--- lib/features/request/screens/list_request.dart
+++ lib/features/request/screens/list_request.dart
@@ -1,87 +1,44 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:bookpals_mobile/core/bases/models/book.dart';
-import 'package:bookpals_mobile/core/theme/color_theme.dart';
-import 'package:bookpals_mobile/core/theme/font_theme.dart';
-import 'package:bookpals_mobile/features/request/models/request.dart';
-import 'package:bookpals_mobile/services/api.dart';
+import 'package:provider/provider.dart';
 
-class RequestPage extends StatefulWidget {
-    const RequestPage({Key? key}) : super(key: key);
+import '../../../core/bases/providers/book_provider.dart';
+import '../../../core/bases/providers/profile_provider.dart';
+import '../../../core/bases/widgets/button.dart';
+import '../../../core/bases/widgets/scaffold.dart';
+import '../../../core/theme/color_theme.dart';
+import '../../authentication/providers/auth_provider.dart';
 
-    @override
-    _RequestPageState createState() => _RequestPageState();
+import '../../authentication/screens/login_page.dart';
+
+class ListRequestPage extends StatefulWidget {
+  const ListRequestPage({super.key});
+
+  @override
+  State<ListRequestPage> createState() => _ListRequestPageState();
 }
 
-class _RequestPageState extends State<RequestPage> {
-Future<List<Request>> fetchProduct() async {
-    var url = Uri.parse(
-        'http://127.0.0.1:8000/get-requested-book/');
-    var response = await http.get(
-        url,
-        headers: {"Content-Type": "application/json"},
+class _ListRequestPageState extends State<ListRequestPage> {
+  @override
+  void initState() {
+    ProfileProvider profileProvider = context.read<ProfileProvider>();
+    profileProvider.setUserProfile();
+    BookProvider bookProvider = context.read<BookProvider>();
+    bookProvider.fetchAllBook();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bookProvider = context.watch<BookProvider>();
+    return ListView.builder(
+      itemCount: bookProvider.listBook.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(bookProvider.listBook[index].fields.name),
+          // Add more customization as needed
+        );
+      },
     );
-
-    // melakukan decode response menjadi bentuk json
-    var data = jsonDecode(utf8.decode(response.bodyBytes));
-
-    // melakukan konversi data json menjadi object Product
-    List<Request> list_request = [];
-    for (var d in data) {
-        if (d != null) {
-            list_request.add(Request.fromJson(d));
-        }
-    }
-    return list_request;
-}
-
-@override
-Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-        title: const Text('Requests'),
-        ),
-        drawer: const LeftDrawer(),
-        body: FutureBuilder(
-            future: fetchProduct(),
-            builder: (context, AsyncSnapshot snapshot) {
-                if (snapshot.data == null) {
-                    return const Center(child: CircularProgressIndicator());
-                } else {
-                    if (!snapshot.hasData) {
-                    return const Column(
-                        children: [
-                        Text(
-                            "Tidak ada request.",
-                            style:
-                                TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                        ),
-                        SizedBox(height: 8),
-                        ],
-                    );
-                } else {
-                    return ListView.builder(
-                        itemCount: snapshot.data!.length,
-                        itemBuilder: (_, index) => Container(
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 12),
-                                padding: const EdgeInsets.all(20.0),
-                                child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                    Text(
-                                    "${snapshot.data![index].fields.name}",
-                                    style: const TextStyle(
-                                        fontSize: 18.0,
-                                        fontWeight: FontWeight.bold,
-                                    ),
-                                    ),
-                                ],
-                                ),
-                            ));
-                    }
-                }
-            }));
-    }
+  }
 }
